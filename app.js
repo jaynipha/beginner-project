ea59df4c1421dfdd3755e391132ee7105bd1c3b5
Jennifer Joseph <josephjenny490@gmail.com>
2022-07-03 18:55:27 +0100

    new changes

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// let x = 3 ^ 35
-
-// console.log(x);
-
-// function sayHello(name){
-//     console.log('hello ' + name)
-// }
-// sayHello('jennifer')
-// var message = 'jenny nwa'
-// console.log(global.message);
-
-
-// function sayHello(name){
-//     window.console.log('omor eh' + name)
-// }
-
 const http = require ('http');
 const server = http.createServer((req, res)=>
 {
